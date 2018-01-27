90197fafb3df22be6a7311e0c1d218a39074a255
huayu-ouyang <ouyang.huayu@gmail.com>
2018-01-27 14:20:30 -0500

    server

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-// var uniqueID = 0 ;
-// var data = {
-//     'jason': {
-//         age: 21,
-//         date: '6/7/96'
-//     },
-//     'mary': {
-//         //...
-//     }
-// }
+var uniqueID = 0 ;
+var data = {
+    'jason': {
+        age: 21,
+        date: '6/7/96'
+    },
+    'mary': {
+        //...
+    }
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 // all this line does is connect a route to a specific file in public folder
@@ -20,17 +20,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'))
 });
 
-// app.post('/', function(req, res) {
-//     uniqueID++;
-//     var someData;
-//     data[someData.name] = someData.information;
-//     res.redirect('/' + someData.name);
-// })
+app.post('/', function(req, res) {
+    uniqueID++;
+   // var someData;
+   // data[someData.name] = someData.information;
+    res.redirect('/' + uniqueID);
+})
 
-// app.get('/:name', function(req, res) {
-//     var name = req.params.name;
-//     res.json(data[name]);
-// })
+app.get('/:uniqueID', function(req, res) {
+    var name = req.params.uniqueID;
+    res.sendFile(path.join(__dirname, '..', 'public/index2.html'))
+    //res.json(data[name]);
+})
 
 app.listen(8000, function () {
   console.log('Server is running on port 8000');
